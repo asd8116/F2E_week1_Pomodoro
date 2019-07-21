8e72104bf38bccd593497908ac8d4fec723385bc
wanaka <asd8116@gmail.com>
2019-07-21 16:27:18 +0800

    Set CheckBox componenet

---
+++ src/components/CheckBox.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="check-box">
+    <input type="checkbox" name id="now-checkbox" />
+    <label class="checkbox_label" for="now-checkbox" :style="{
+        width: width + 'px',
+        height: height + 'px'
+      }">
+      <span class="hook" :style="{ fontSize: size + 'px' }">&#10003;</span>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      default: 48
+    },
+    height: {
+      default: 48
+    },
+    size: {
+      default: 32
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.check-box {
+  input {
+    display: none;
+    &:checked {
+      ~ label.checkbox_label .hook {
+        opacity: 1;
+      }
+    }
+  }
+  label.checkbox_label {
+    display: block;
+    border: 2px solid $primary-color;
+    border-radius: 50%;
+    cursor: pointer;
+    margin-right: 1rem;
+    .hook {
+      position: absolute;
+      opacity: 0;
+      box-sizing: border-box;
+      background: transparent;
+      color: $primary-color;
+      line-height: 1;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      transition: border-color 0.2s ease-in-out, opacity 0.2s ease-in-out;
+    }
+  }
+}
+</style> 
--- src/components/home/TimePanel.vue
+++ src/components/home/TimePanel.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="time_panel">
     <div class="now_mession_title">
-      <div class="check-box">
-        <input type="checkbox" name id="now-checkbox" />
-        <label class="checkbox_label" for="now-checkbox"></label>
-      </div>
+      <check-box></check-box>
+
       <div class="now_mession">
         <div class="title">the First thing to do today</div>
         <div class="dot"></div>
@@ -15,7 +13,13 @@
 </template>
 
 <script>
-export default {};
+import CheckBox from "../CheckBox.vue";
+
+export default {
+  components: {
+    CheckBox
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -23,37 +27,6 @@ export default {};
   width: 100%;
   .now_mession_title {
     display: flex;
-    .check-box input {
-      display: none;
-      &:checked {
-        ~ label.checkbox_label::after {
-          opacity: 1;
-        }
-      }
-    }
-    label.checkbox_label {
-      display: block;
-      width: 48px;
-      height: 48px;
-      border: 2px solid $primary-color;
-      border-radius: 50%;
-      cursor: pointer;
-      margin-right: 1rem;
-      &:after {
-        position: absolute;
-        opacity: 0;
-        content: "\2713";
-        box-sizing: border-box;
-        background: transparent;
-        color: $primary-color;
-        font-size: 2rem;
-        line-height: 1;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        transition: border-color 0.2s ease-in-out, opacity 0.2s ease-in-out;
-      }
-    }
     .now_mession {
       .title {
         font-size: 1.5rem;

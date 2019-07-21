aeee2d8f0e0aa8236e001b4bfaeb28b245c75138
wanaka <asd8116@gmail.com>
2019-07-21 14:47:01 +0800

    Init scss setting

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,15 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.about {
+  h1 {
+    color: $color-yellow;
+  }
+}
+</style> 

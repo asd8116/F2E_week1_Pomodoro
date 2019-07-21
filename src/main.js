cfaf822572c8a7eef68b429c2ea475a9b706d3bc
wanaka <asd8116@gmail.com>
2019-07-21 14:56:22 +0800

    Install vue-page-transition

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 // import "./registerServiceWorker";
+import VuePageTransition from "vue-page-transition";
 
+Vue.use(VuePageTransition);
 Vue.config.productionTip = false;
 
 new Vue({
